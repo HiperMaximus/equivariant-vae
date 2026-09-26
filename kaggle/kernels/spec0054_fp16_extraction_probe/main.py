93988995814e20fb1ab7c3f5430484f9adb4d3c8
HiperMaximus <hipermaximus@gmail.com>
2026-09-26 13:00:02 -0500

    experiment: launch AMP latent shard 12

--- kaggle/kernels/spec0054_fp16_extraction_probe/main.py
+++ kaggle/kernels/spec0054_fp16_extraction_probe/main.py
@@ -1,5 +1,5 @@
 # Copyright 2026 HiperMaximus
-"""Thin Kaggle entrypoint for the eleventh latent shard."""
+"""Thin Kaggle entrypoint for the twelfth latent shard."""
 
 import subprocess
 import sys
@@ -61,7 +61,7 @@ def main() -> int:
     return run(
         repo_root=SOURCE_ROOT,
         source_commit=commit,
-        shard_index=11,
+        shard_index=12,
     )
 
 
